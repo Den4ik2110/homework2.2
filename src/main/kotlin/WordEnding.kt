ecc00654d93c1e9ceb17247c6235b07d2160a600
Den4ik2110 <93542301+Den4ik2110@users.noreply.github.com>
2022-04-08 20:26:35 +0500

    Format code

--- src/main/kotlin/WordEnding.kt
+++ src/main/kotlin/WordEnding.kt
@@ -1,6 +1,6 @@
 fun main() {
     var like = 10
-    if (like%10 == 1 && like != 11) {
+    if (like % 10 == 1 && like != 11) {
         println("Понравилось $like человеку")
     } else {
         println("Понравилось $like человекам")
