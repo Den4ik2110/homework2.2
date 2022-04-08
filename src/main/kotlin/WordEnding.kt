1c10b12ae64f3835850001d76d9382a7486620de
Денис Бобровский <bdj.211097@gmail.com>
2022-04-08 20:20:13 +0500

    First commit

---
+++ src/main/kotlin/WordEnding.kt
@@ -0,0 +1,8 @@
+fun main() {
+    var like = 10
+    if (like%10 == 1 && like != 11) {
+        println("Понравилось $like человеку")
+    } else {
+        println("Понравилось $like человекам")
+    }
+}
